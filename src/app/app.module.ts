7fc192a757465da3abca6404ba223a764ce85f5e
KacperAa <kacperaugvstyn@gmail.com>
2023-08-18 09:41:10 +0200

    patch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { JobsListElComponent } from './components/jobs-list-el/jobs-list-el.comp
 import { CategoriesComponent } from './components/categories/categories.component';
 import { BrandDataComponent } from './components/brand-data/brand-data.component';
 import { GraphicThemeComponent } from './components/graphic-theme/graphic-theme.component';
+import { JobFilterComponent } from './components/job-filter/job-filter.component';
 
 @NgModule({
-  declarations: [AppComponent, JobsOffersComponent, JobsListElComponent, CategoriesComponent, BrandDataComponent, GraphicThemeComponent],
+  declarations: [AppComponent, JobsOffersComponent, JobsListElComponent, CategoriesComponent, BrandDataComponent, GraphicThemeComponent, JobFilterComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
