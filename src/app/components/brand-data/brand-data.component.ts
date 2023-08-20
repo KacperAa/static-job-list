88fc9f80bf89b1ebd14d402d1ad708f633088eed
KacperAa <kacperaugvstyn@gmail.com>
2023-08-20 11:26:36 +0200

    patch

--- src/app/components/brand-data/brand-data.component.ts
+++ src/app/components/brand-data/brand-data.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Job } from 'src/app/models/job.interface';
 
 @Component({
   selector: 'app-brand-data',
   templateUrl: './brand-data.component.html',
-  styleUrls: ['./brand-data.component.scss']
+  styleUrls: ['./brand-data.component.scss'],
 })
 export class BrandDataComponent {
-
+  @Input({ required: true }) public jobData!: Job;
 }
