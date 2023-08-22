affaafa2dbfea800a477a3c88beea1efce185f98
KacperAa <kacperaugvstyn@gmail.com>
2023-08-22 14:14:11 +0200

    patch

--- src/app/components/job-filter/job-filter.component.ts
+++ src/app/components/job-filter/job-filter.component.ts
@@ -1,8 +1,21 @@
 import { Component, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
   selector: 'app-job-filter',
   templateUrl: './job-filter.component.html',
   styleUrls: ['./job-filter.component.scss'],
 })
-export class JobFilterComponent {}
+export class JobFilterComponent {
+  public selectedCategories$!: Observable<string[]>;
+  constructor(private _categoriesService: CategoriesService) {
+    this._getSelectedCategories();
+  }
+
+  private _getSelectedCategories() {
+    const selectedCategories$ =
+      this._categoriesService.selectedCategories$.asObservable();
+    this.selectedCategories$ = selectedCategories$;
+  }
+}
