2c5c5e6e798cf4874c12b92b6f82af4ff1d4a387
KacperAa <kacperaugvstyn@gmail.com>
2023-08-22 14:10:21 +0200

    patch

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,15 +1,8 @@
-import {
-  Component,
-  ElementRef,
-  EventEmitter,
-  Input,
-  Output,
-  QueryList,
-  ViewChild,
-  ViewChildren,
-} from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { map, subscribeOn } from 'rxjs';
 
 import { Job } from 'src/app/models/job.interface';
+import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
   selector: 'app-categories',
@@ -18,12 +11,18 @@ import { Job } from 'src/app/models/job.interface';
 })
 export class CategoriesComponent {
   @Input({ required: true }) public jobData!: Job;
+  public selectedCategories: string[] = [];
 
-  @Output() public categoryEmitter = new EventEmitter<string>();
+  constructor(private _categoriesService: CategoriesService) {}
 
   public captureCategory(inputEl: HTMLInputElement) {
     if (inputEl.checked) {
-      this.categoryEmitter.emit(inputEl.value);
+      const selectedCategories$ = this._categoriesService.selectedCategories$;
+      selectedCategories$.next([
+        ...selectedCategories$.getValue(),
+        inputEl.value,
+      ]);
+      selectedCategories$.subscribe((res) => console.log(res));
     }
   }
 }
