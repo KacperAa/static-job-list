e3e868484d0e7d0eb7a7a3736040d330a9fb2619
KacperAa <kacperaugvstyn@gmail.com>
2023-08-24 14:20:51 +0200

    patch

---
+++ src/app/UI/assign-frontend-mentor/assign-frontend-mentor.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-assign-frontend-mentor',
+  templateUrl: './assign-frontend-mentor.component.html',
+  styleUrls: ['./assign-frontend-mentor.component.scss'],
+})
+export class AssignFrontendMentorComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BrandDataComponent } from './components/brand-data/brand-data.component
 import { GraphicThemeComponent } from './components/graphic-theme/graphic-theme.component';
 import { JobFilterComponent } from './components/job-filter/job-filter.component';
 import { LoadingCircleComponent } from './UI/loading-circle/loading-circle.component';
+import { AssignFrontendMentorComponent } from './UI/assign-frontend-mentor/assign-frontend-mentor.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { LoadingCircleComponent } from './UI/loading-circle/loading-circle.compo
     GraphicThemeComponent,
     JobFilterComponent,
     LoadingCircleComponent,
+    AssignFrontendMentorComponent,
   ],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
